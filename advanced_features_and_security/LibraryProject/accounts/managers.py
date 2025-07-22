721aff3fe7a9441a758ffd2137bfb29335d5dae5
Malilo Sandra <161347233+uughty@users.noreply.github.com>
2025-07-23 00:35:59 +0300

    Add CustomUserManager and connect it to CustomUser model

---
+++ advanced_features_and_security/LibraryProject/accounts/managers.py
@@ -0,0 +1,33 @@
+from django.contrib.auth.models import BaseUserManager
+
+class CustomUserManager(BaseUserManager):
+    def create_user(self, username, email, date_of_birth, password=None, **extra_fields):
+        if not email:
+            raise ValueError('Users must have an email address')
+        if not username:
+            raise ValueError('Users must have a username')
+        if not date_of_birth:
+            raise ValueError('Users must have a date of birth')
+
+        email = self.normalize_email(email)
+        user = self.model(
+            username=username,
+            email=email,
+            date_of_birth=date_of_birth,
+            **extra_fields
+        )
+        user.set_password(password)
+        user.save(using=self._db)
+        return user
+
+    def create_superuser(self, username, email, date_of_birth, password=None, **extra_fields):
+        extra_fields.setdefault('is_staff', True)
+        extra_fields.setdefault('is_superuser', True)
+        extra_fields.setdefault('is_active', True)
+
+        if extra_fields.get('is_staff') is not True:
+            raise ValueError('Superuser must have is_staff=True.')
+        if extra_fields.get('is_superuser') is not True:
+            raise ValueError('Superuser must have is_superuser=True.')
+
+        return self.create_user(username, email, date_of_birth, password, **extra_fields)
