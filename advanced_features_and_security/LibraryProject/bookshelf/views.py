fee2c833729ccc59351a5eb6e2ce2014f52ffa93
Malilo Sandra <161347233+uughty@users.noreply.github.com>
2025-07-24 20:36:51 +0300

    Add book_list view with permission_required decorator

--- advanced_features_and_security/LibraryProject/bookshelf/views.py
+++ advanced_features_and_security/LibraryProject/bookshelf/views.py
@@ -1,23 +1,23 @@
+from django.contrib.auth.decorators import permission_required
 from django.shortcuts import render
-from django.contrib.auth.decorators import login_required, permission_required
-from django.http import HttpResponse
+from .models import Book
 
-@login_required
 @permission_required('bookshelf.can_view', raise_exception=True)
-def view_dashboard(request):
-    return HttpResponse("Viewing dashboard.")
+def book_list(request):
+    books = Book.objects.all()
+    return render(request, 'bookshelf/book_list.html', {'books': books})
 
-@login_required
 @permission_required('bookshelf.can_create', raise_exception=True)
-def create_item(request):
-    return HttpResponse("Item created.")
+def book_create(request):
+    # Form handling to create a book
+    pass
 
-@login_required
 @permission_required('bookshelf.can_edit', raise_exception=True)
-def edit_item(request):
-    return HttpResponse("Item edited.")
+def book_edit(request, book_id):
+    # Form handling to edit a book
+    pass
 
-@login_required
 @permission_required('bookshelf.can_delete', raise_exception=True)
-def delete_item(request):
-    return HttpResponse("Item deleted.")
+def book_delete(request, book_id):
+    # Form handling to delete a book
+    pass
