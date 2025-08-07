5d4ea7f78c1863e64d4c6b726ae26a2c0ef52c51
Malilo Sandra <161347233+uughty@users.noreply.github.com>
2025-08-07 12:19:30 +0300

    🔐 Add role-based permissions & secure API endpoints

--- advanced-api-project/api/permissions.py
+++ advanced-api-project/api/permissions.py
@@ -1,18 +1,11 @@
-# api/permissions.py
-
 from rest_framework.permissions import BasePermission
 
 class IsAdminOrEditor(BasePermission):
     """
-    Custom permission to allow only users in 'Admins' or 'Editors' groups.
+    Custom permission to allow only Admins or Editors to access.
     """
-
     def has_permission(self, request, view):
-        return (
-            request.user
-            and request.user.is_authenticated
-            and (
-                request.user.groups.filter(name="Admins").exists()
-                or request.user.groups.filter(name="Editors").exists()
-            )
+        return request.user and (
+            request.user.groups.filter(name='Admins').exists() or
+            request.user.groups.filter(name='Editors').exists()
         )
