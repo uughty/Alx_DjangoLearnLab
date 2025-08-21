97611271c1eff69be6aca9d05ef3bb92ac05dba5
Malilo Sandra <161347233+uughty@users.noreply.github.com>
2025-08-21 22:00:20 +0300

    Implemented posts and comments functionality with CRUD, serializers, views, and routes

--- social_media_api/social_media_api/settings.py
+++ social_media_api/social_media_api/settings.py
@@ -41,12 +41,15 @@ INSTALLED_APPS = [
     'rest_framework',
     'rest_framework.authtoken',
     'accounts',
+    'posts',
 ]
 
 REST_FRAMEWORK = {
     'DEFAULT_AUTHENTICATION_CLASSES': [
         'rest_framework.authentication.TokenAuthentication',
     ],
+    'DEFAULT_PAGINATION_CLASS': 'rest_framework.pagination.PageNumberPagination',
+    'PAGE_SIZE': 5,  # adjust as needed
 }
 
 MIDDLEWARE = [
--- social_media_api/social_media_api/urls.py
+++ social_media_api/social_media_api/urls.py
@@ -3,5 +3,6 @@ from django.urls import path, include
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('api/accounts/', include('accounts.urls')),  # ✅ includes accounts URLs
+    path('api/accounts/', include('accounts.urls')), 
+     path('api/', include('posts.urls')), # ✅ includes accounts URLs
 ]
