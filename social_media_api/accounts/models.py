010e07d83260fc15b502096247c4cb4e56a88428
Malilo Sandra <161347233+uughty@users.noreply.github.com>
2025-08-22 13:55:10 +0300

    Add custom user model with bio, profile_picture, and followers

--- social_media_api/accounts/models.py
+++ social_media_api/accounts/models.py
@@ -8,3 +8,9 @@ class CustomUser(AbstractUser):
         related_name='followers',
         blank=True
     )
+class CustomUser(AbstractUser):
+    bio = models.TextField(blank=True, null=True)
+    profile_picture = models.ImageField(upload_to="profile_pics/", blank=True, null=True)
+    followers = models.ManyToManyField("self", symmetrical=False, related_name="following", blank=True)
+    def __str__(self):
+        return self.username
